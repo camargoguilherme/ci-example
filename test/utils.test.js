b2556f0534341ab47f4ab2caf697688fc5436d7f
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2021-10-18 19:37:48 -0300

    test: adicionando teste para 'acharCaracter'

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,19 +1,41 @@
 const { gerarNumeroAleatorio, acharCaracter } = require("../lib/utils");
 
 describe("Utils", () => {
-  test("inicio nao pode ser negativo", () => {
-    expect(gerarNumeroAleatorio(-4, 16)).toBe(-1);
-  });
-
   describe("gerarNumeroAleatorio", () => {
+    test("inicio nao pode ser negativo", () => {
+      expect(gerarNumeroAleatorio(-4, 16)).toBe(-1);
+    });
+
     test("fim nao pode ser negativo", () => {
       expect(gerarNumeroAleatorio(20, -5)).toBe(-1);
+    });
+
+    test("inicio maior que o fim", () => {
+      expect(gerarNumeroAleatorio(20, 12)).toBe(-1);
+    });
+
+    test("inicio igual ao fim", () => {
+      expect(gerarNumeroAleatorio(20, 20)).toBe(-1);
     });
   });
 
   describe("acharCaracter", () => {
     test("comprimento invalido", () => {
       expect(acharCaracter(-4, "abcde", "c")).toBe("comprimento invalido");
+    });
+
+    test("comprimento invalido", () => {
+      expect(acharCaracter(6, "abcde", "c")).toBe(
+        "comprimento fornecido diferente do comprimento real"
+      );
+    });
+
+    test("dados válidos e não encontra o caracter", () => {
+      expect(acharCaracter(6, "qwerty", "c")).toBe("caracter encontrado");
+    });
+
+    test("dados válidos e encontra o caracter", () => {
+      expect(acharCaracter(5, "abcde", "c")).toBe(2);
     });
   });
 });
